ebebb80238bd1aa60cbc3a5db7199b2f94c3fc80
An525ish <an525ish@gmail.com>
2023-05-17 19:51:48 +0530

    added build

--- src/Components/SearchResultVideoCard.jsx
+++ src/Components/SearchResultVideoCard.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { abbreviateNumber } from "js-abbreviation-number";
 import { BsFillCheckCircleFill } from "react-icons/bs";
